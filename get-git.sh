522ffdda8d93f63cf966e392f5cad5f3cc53855d
funixz <7918248+funixz@users.noreply.github.com>
2019-01-02 11:52:47 -0500

    get-git.sh: cleanup & fixes

--- get-git.sh
+++ get-git.sh
@@ -10,10 +10,12 @@
 
 #global variables
 
-cgitver=v$(git --version|cut -d ' ' -f 3)
+cgitver="v$(/usr/local/bin/git --version|cut -d ' ' -f 3)"
+wgit=$(which git)
 #cgitver="v2.16.0-rc1" #for testing
-latest=$(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 2 | head -n 1 | cut -d \/ -f 5 |sed 's/.tar.gz//')
-wdir=$(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 2 | head -n 1 | cut -d \/ -f 5 |sed 's/v//' |sed 's/.tar.gz//')
+#latest=$(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 2 | head -n 1 | cut -d \/ -f 5 |sed 's/.tar.gz//')
+latest=$(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 4 | head -n 1 | cut -d \/ -f 5 |sed 's/.tar.gz//')
+wdir=$(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 4 | head -n 1 | cut -d \/ -f 5 |sed 's/v//' |sed 's/.tar.gz//')
 OS=""
 
 #functions
@@ -26,53 +28,59 @@ sleep 1s
 # up next
 #;}
 
+gitreport() {
+	cgitver="v$(/usr/local/bin/git --version|cut -d ' ' -f 3)"
+	wgit="/usr/local$(which git)"
+	echo -e "\nCurrent installed Git version is now $cgitver, found at $wgit .\n"
+}
+
 upgradegitDeb() {
-echo ""; echo "Upgrading Git..."
-echo "Going from -- $cgitver -- to -- $latest --. "; echo ""
+echo -e "\nUpgrading Git..."
+echo "Going from -- $cgitver -- to -- $latest --. \n"
 newlink="https://github.com/git/git/archive/$latest.tar.gz"
 #-----
 echo "pulling from $newlink"
 cd /tmp && wget -q --show-progress $newlink
 s2;
-cd /tmp && tar -xzf $(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 2 | head -n 1 | cut -d \/ -f 5)
+cd /tmp && tar -xzf $(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 4 | head -n 1 | cut -d \/ -f 5)
 s2
-cd /tmp/git-$wdir && make configure >> /tmp/git-from-source.log 2>&1
+cd /tmp/git-$wdir && make --silent configure >> /tmp/git-from-source.log 2>&1
 s2
 cd /tmp/git-$wdir && ./configure
 s2
-cd /tmp/git-$wdir && make prefix=/usr/local all > /tmp/git-from-source.log 2>&1
+cd /tmp/git-$wdir && make --silent prefix=/usr/local all > /tmp/git-from-source.log 2>&1
 s2
-cd /tmp/git-$wdir && make prefix=/usr/local install > /tmp/git-from-source.log 2>&1
+cd /tmp/git-$wdir && make --silent prefix=/usr/local install > /tmp/git-from-source.log 2>&1
 s2
-which git
 #-----
 
-echo ""; echo "Done."; echo "Current installed Git version is now $cgitver ."; echo ""
+gitreport;
+
 }
 
 upgradegitCE() {
-echo ""; echo "Installing dependencies.."
-yum groupinstall -yq "Development Tools"
-yum install -yq wget gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel gcc autoconf
-echo ""; echo "Upgrading Git..."
-echo "Going from -- $cgitver -- to -- $latest --. "; echo ""
+echo -e "\nInstalling dependencies.."
+yum -q groupinstall -y "Development Tools"
+yum -q install -y wget gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel gcc autoconf
+echo -e "\nUpgrading Git..."
+echo -e "Going from -- $cgitver -- to -- $latest --. \n"
 newlink="https://github.com/git/git/archive/$latest.tar.gz"
 #-----
 echo "pulling from $newlink"
 cd /tmp && wget -q --progress=bar:force $newlink
 s2;
-cd /tmp && tar -xzf $(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 2 | head -n 1 | cut -d \/ -f 5)
+cd /tmp && tar -xzf $(curl https://github.com/git/git/releases -s | grep archive | grep '.tar.gz' | cut -d \" -f 4 | head -n 1 | cut -d \/ -f 5)
 s2
-cd /tmp/git-$wdir && make configure >> /tmp/git-from-source.log 2>&1
+cd /tmp/git-$wdir && make --silent configure >> /tmp/git-from-source.log 2>&1
 s2
 cd /tmp/git-$wdir && ./configure --prefix=/usr/local
 s2
-cd /tmp/git-$wdir && make install > /tmp/git-from-source.log 2>&1
+cd /tmp/git-$wdir && make --silent install > /tmp/git-from-source.log 2>&1
 s2
-which git
 #-----
 
-echo ""; echo "Done."; echo "Current installed Git version is now $cgitver ."; echo ""
+gitreport;
+
 }
 
 #main
@@ -92,12 +100,11 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ -n "$1" ]; then
   echo "- run with sudo/root"
   echo "- make sure there's no prior git related files/folders in /tmp before running."
   echo "- currently accepts no CLI parameters, except '-h' or '--help'"
-  echo "--------------------------------------------------------------------------------"
-  echo ""
+  echo -e "--------------------------------------------------------------------------------\n"
 else
 
  echo "Current installed Git version is $cgitver ."
- echo "Latest Git on github.com is $latest"; echo ""
+ echo -e "Latest Git on github.com is $latest \n"
 
  if [ "$cgitver" == "$latest" ]; then
     echo "Current Git matches latest available. Exiting."
