0fd75726fed0174709173777b6d3060a527fac2a
Aren <7918248+funixz@users.noreply.github.com>
2020-09-03 23:37:27 -0400

    variables added

--- setup-teamd-lacp.sh
+++ setup-teamd-lacp.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 #
-# Setup team0 via nmcli
+# Setup team0 via nmcli.
 # This needs to be run on the system itself unless you've a third interface to SSH in with.
 # This sets up two interfaces in LACP mode, assuming the switch is already prepared for it.
-# Tested with a Procurve 1800-24g switch with LACP enabled on the ports. (NOT Trunking)
-# DO CHECK THE WHOLE SCRIPT before using, ensuring values are appropriate
+# Tested with a Procurve 1800-24g switch with LACP enabled on the ports (NOT Trunking).
+# DO CHECK THE WHOLE SCRIPT before using, ensuring values are appropriate/
+# Assumes to be run with root privileges for package install and network changes.
+# Assumes the intended interfaces and names are nonexistent.
 ##
 
 ETH1="enp2s0f0"       # interface 1
 ETH2="enp2s0f1"       # interface 2
 TEAM="team0"          # desired name for the teamd interface
 TYPE="lacp"           # the mode for the teamd interface (this script assumed for lacp)
-SYSIP="192.168.1.XXX"  # ip of the server
+SYSIP="x.x.x.x"  # ip of the server
+MASK="24"        # network mask (eg: 24 == 255.255.255.0)
+GW="x.x.x.x"     # your gateway IP
+DNSIP="x.x.x.x"  # your DNS server
+SDNS="x.xyz.x"   # your DNS domain
+
+
+# install necessary packages
+yum install teamd NetworkManager-team -y
 
 # first, drop the ethernet connections we want to use. This is what the docs didn't tell us.
 
@@ -29,10 +39,10 @@ nmcli connection add type ethernet slave-type team con-name ${TEAM}-port1 ifname
 nmcli connection add type ethernet slave-type team con-name ${TEAM}-port2 ifname ${ETH2} master ${TEAM}
 
 # setup the addressing for the team
-nmcli connection modify ${TEAM} ipv4.addresses '${SYSIP}/24' # assumes the /24 netmask
-nmcli connection modify ${TEAM} ipv4.gateway '192.168.1.X'  # your gateway IP
-nmcli connection modify ${TEAM} ipv4.dns '192.168.1.X'  # your DNS server
-nmcli connection modify ${TEAM} ipv4.dns-search 'example.net'  # your dns domain
+nmcli connection modify ${TEAM} ipv4.addresses ${SYSIP}/${MASK} # ip/netmask
+nmcli connection modify ${TEAM} ipv4.gateway ${GW}  # your gateway IP
+nmcli connection modify ${TEAM} ipv4.dns ${DNSIP}  # your DNS server
+nmcli connection modify ${TEAM} ipv4.dns-search ${SDNS}  # your dns domain
 nmcli connection modify ${TEAM} ipv4.method manual  # this is a static IP
 
 # activate the team
