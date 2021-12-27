8cf047f77d07d2903ffab822ff29e4b261334d83
Aren <aren.dej@gmail.com>
2021-12-27 16:11:59 -0500

    Update updatewifi.sh

--- updatewifi.sh
+++ updatewifi.sh
@@ -4,16 +4,26 @@
 # It's not super convenient but that's why I made this script to run if a
 # kernel update happens.
 
-CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
+# change the user var to your own.
+# depends on the repo having been cloned already.
 
+MYUSER=adj
+
+CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
 if [ ${CAN_I_RUN_SUDO} -gt 0 ]
 then
    echo -e "\nSudo available..\nUpdating wifi driver."
-   if [[ "$1" == "--offline" || "$1" == "-offline" ]]; 
+   if [[ "$1" == "--offline" || "$1" == "-offline" ]];
     then
-     cd /home/adj/rtw89 && make && sudo make install
+     mkdir -p /home/$MYUSER/rtw89 && cd /home/$MYUSER/rtw89
+     make clean
+     make && sudo make install
+     sudo modprobe rtw89pci
     else
-     cd /home/adj/rtw89 && git pull && make && sudo make install
+     mkdir -p /home/$MYUSER/rtw89 && cd /home/$MYUSER/rtw89
+     make clean
+     git pull && make && sudo make install
+     sudo modprobe rtw89pci
    fi
 else
     echo "Sudo not available.. exiting"
